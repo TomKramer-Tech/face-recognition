5d210b03741a5d4183d33f295607c354b525ff28
Tom Kramer <thomaswkramer@outlook.com>
2022-11-08 22:56:59 -0600

    front end finished.

--- src/components/faceRec/FaceRecognition.js
+++ src/components/faceRec/FaceRecognition.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-const FaceRecognition = ({ imageURL }) => {
+import './FaceRecognition.css'
+const FaceRecognition = ({ imageURL, box }) => {
     return(
         <div className='center ma'>
             <div className='absolute mt2'>
-              <img alt='user defined.' width='500px' height='auto' src={imageURL} />
+              <img id='inputimage' alt='user defined.' width='500px' height='auto' src={imageURL} />
+              <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Navigation = () => {
-    return(
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa pointer'>Sign Out</p>
-        </nav>
-    );
-}
 
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    
+    if (isSignedIn) {
+        return (
+          <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa pointer'>Sign Out</p>
+          </nav>
+        );
+    }   else {
+        return (
+          <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signIn')} className='f3 pa3 link dim black underline pa pointer'>Sign In</p>
+            <p onClick={() => onRouteChange('register')} className='f3 pa3 link dim black underline pa pointer'>Register</p>
+          </nav>
+        );
+    }
+}
 export default Navigation;
